7b5bcb37d0df0b688d2c9cc55f7f4fb0c572af2a
Vinh Tran <vinhtran.k@gmail.com>
2020-11-24 22:26:33 -0600

    created accountInfo.js

---
+++ accountinfo.js
@@ -0,0 +1,13 @@
+checkInput = function() {
+	var name = document.getElementById("nameInfo").value;
+	var email = document.getElementById("emailInfo").value;
+	var password = document.getElementById("passwordInfo").value;
+	var text;
+	
+	if (name === '' || name == null) {
+		alert("ayo u need a name");
+		return false;
+	}
+	
+}
+	

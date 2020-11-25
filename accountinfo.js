d62916bd31618a8acc3e24fd0fa642f446765661
Vinh Tran <vinhtran.k@gmail.com>
2020-11-24 23:00:33 -0600

    input validation for accountinfo

--- accountinfo.js
+++ accountinfo.js
@@ -2,12 +2,31 @@ checkInput = function() {
 	var name = document.getElementById("nameInfo").value;
 	var email = document.getElementById("emailInfo").value;
 	var password = document.getElementById("passwordInfo").value;
-	var text;
+	
+	var errorMsg = "Signup Failed. Missing field(s):";
+	
+	var valid = true;
 	
 	if (name === '' || name == null) {
-		alert("ayo u need a name");
-		return false;
+		errorMsg = errorMsg.concat(" Name");
+		valid = false;
 	}
+	
+	if(email === "" || email == null) {
+		errorMsg = errorMsg.concat(" Email");
+		valid = false;
+	}
+	
+	if(password === "" || password == null) {
+		errorMsg = errorMsg.concat(" Password");
+		valid = false;
+	}
+	
+	if(!valid){
+		alert(errorMsg);
+	}
+	
+	return valid;
 	
 }
 	
